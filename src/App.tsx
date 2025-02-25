c4c3e97a8fe34c0da338d36af0a8137f4cbf2035
AdriellyN <adriellynr@gmail.com>
2025-02-25 11:56:37 -0300

    :sparkles: CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import Footer from './components/footer/Footer'
+import ListarCategorias from './components/categorias/listacategorias/ListarCategorias'
+import FormCategoria from './components/categorias/formcategoria/FormCategoria'
+import DeletarCategoria from './components/categorias/deletarcategoria/DeletarCategoria'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Home />
-        <Footer/>
+        <div className="flex flex-col min-h-screen bg-gray-200 ">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListarCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   )
---
+++ src/components/categorias/deletarcategoria/DeletarCategoria.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Categoria from "../../../models/Categoria";
+import { buscar, deletar } from "../../../services/Service";
+import { Check, X } from "@phosphor-icons/react";
+import { RotatingLines } from "react-loader-spinner";
+
+function DeletarCategoria() {
+
+    const navigate = useNavigate()
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
+
+    const { id } = useParams<{ id: string }>();
+
+    async function buscarPorId(id: string) {
+        try {
+            await buscar(`/categorias/${id}`, setCategoria)
+        } catch (error: any) {
+            alert("Categoria não encontrada!")
+        }
+    }
+
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id)
+        }
+    }, [id])
+
+    async function deletarCategoria() {
+        setIsLoading(true)
+
+        try {
+            await deletar(`/categorias/${id}`)
+
+            alert("Categoria apagada com sucesso")
+
+        } catch (error) {
+            alert("Erro ao apagar a categoria")
+        }
+
+        setIsLoading(false)
+        retornar()
+    }
+
+    function retornar() {
+        navigate("/categorias")
+    }
+
+    return (
+        <div className="container w-1/3 mx-auto">
+            <h1 className="py-4 text-4xl text-center">Deletar Categoria</h1>
+            <p className="mb-4 font-semibold text-center">
+                Você tem certeza de que deseja apagar a categoria a seguir?</p>
+            <div className="flex flex-col justify-between overflow-hidden border rounded-2xl">
+                <header
+                    className="px-6 py-2 text-2xl font-bold text-white bg-slate-600">
+                    Categoria
+                </header>
+                <p className="h-full p-8 text-3xl bg-white">{categoria.nome}</p>
+                <div className="flex">
+                    <button
+                        className="w-full py-2 bg-red-400 text-slate-50 hover:bg-red-700"
+                        onClick={retornar}
+                    >
+                        <X size={48} color="#ffffff" />
+                    </button>
+                    <button
+                        className="flex items-center justify-center w-full bg-indigo-400 text-slate-50 hover:bg-indigo-700"
+                        onClick={deletarCategoria}
+                    >
+                        {isLoading ?
+                            <RotatingLines
+                                strokeColor="white"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                width="24"
+                                visible={true}
+                            /> :
+                            <span>
+                                <Check size={48} color="#ffffff" />
+                            </span>
+                        }
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default DeletarCategoria
+
